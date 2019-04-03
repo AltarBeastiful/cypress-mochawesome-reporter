db065e43de1b04729c15e46c7dbb105c046f12b1
Rémi Benoit <r3m1.benoit@gmail.com>
2019-04-03 23:56:05 +0200

    WIP Add failed screenshots to report

--- cypress/integration/sample1.spec.js
+++ cypress/integration/sample1.spec.js
@@ -1,6 +1,15 @@
 /// <reference types="Cypress" />
 
 context('Sample test', () => {
+    const addContext = require('mochawesome/addContext');
+
+
+Cypress.on('test:after:run', (test, runnable) => {
+    if(test.state == 'failed') {
+
+        addContext({ test }, `./screenshots/${runnable.parent.title} -- ${test.title} (failed).png`)
+    }
+});
   
     it('Passed', () => {
         cy.document().title().should('not.contain', 'unicorns')
