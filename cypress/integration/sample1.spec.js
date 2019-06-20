1dff4bc330d7c0da89c64965c4d00c0e2ea2fcdf
Rémi Benoit <r3m1.benoit@gmail.com>
2019-06-20 15:15:00 +0200

    Update cypress and mocha
    Ignore screenshots, reports and videos
    Include screenshots with correct path

--- cypress/integration/sample1.spec.js
+++ cypress/integration/sample1.spec.js
@@ -4,20 +4,18 @@ context('Sample test', () => {
     const addContext = require('mochawesome/addContext');
 
 
-Cypress.on('test:after:run', (test, runnable) => {
-    if(test.state == 'failed') {
+    Cypress.on('test:after:run', (test, runnable) => {
+        if(test.state == 'failed') {
+            addContext({ test }, `../cypress/screenshots/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`)
+        }
+    });
 
-        addContext({ test }, `./screenshots/${runnable.parent.title} -- ${test.title} (failed).png`)
-    }
-});
-  
     it('Passed', () => {
         cy.document().title().should('not.contain', 'unicorns')
     })
-  
+
     it('Failing', () => {
         cy.document().title().should('contain', 'unicorns')
     })
 
   })
-  
