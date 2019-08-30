dc26052c035ce9946b81e0a28f1fa33e4a2d5a3f
Rémi Benoit <r3m1.benoit@gmail.com>
2019-08-30 15:44:20 +0200

    Record runs on cypress dashboard to compare

--- scripts/cypress.js
+++ scripts/cypress.js
@@ -5,7 +5,10 @@ const generator = require('mochawesome-report-generator')
 
 async function runTests() {
   await fse.remove('mochawesome-report')
-  const { totalFailed } = await cypress.run()
+  const { totalFailed } = await cypress.run({
+    record: true,
+    key: "341971c0-3c21-4d14-9f0b-59d8316090fc"
+  })
   const jsonReport = await merge()
   await generator.create(jsonReport)
   process.exit(totalFailed)
